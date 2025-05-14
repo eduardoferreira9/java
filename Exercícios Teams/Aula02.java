66451987fb24a74777d80807515548fa1d3d193d
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2025-05-14 18:56:23 -0300

    fix: add private method

--- Exercícios Teams/Aula02.java
+++ Exercícios Teams/Aula02.java
@@ -1,11 +1,59 @@
 class Pessoa {
-    public String nome;
-    public String email;
-    public String telefone;
-    public int idade;
-    public char sexo;
-    public String cpf;
+    private String nome;
+    private String email;
+    private String telefone;
+    private int idade;
+    private char sexo;
+    private String cpf;
 
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
     void exibirDados() {
         System.out.println("Nome: " + this.nome);
@@ -18,38 +66,72 @@ class Pessoa {
 }
 
 class Professor extends Pessoa {
-    public double salario;
-    public String disciplina;
+    private double salario;
+    private String disciplina;
+
+    // Getters e Setters
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
 
     void adicionarProfessor(String nome, String email, String telefone, int idade, char sexo, String cpf, double salario, String disciplina) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.idade = idade;
-        this.sexo = sexo;
-        this.cpf = cpf;
+        setNome(nome);
+        setEmail(email);
+        setTelefone(telefone);
+        setIdade(idade);
+        setSexo(sexo);
+        setCpf(cpf);
         this.salario = salario;
         this.disciplina = disciplina;
     }
 
     void exibirDados() {
         super.exibirDados();
-        System.out.println("Salário:" + this.salario);
-        System.out.println("Disciplinas: " + this.disciplina);
+        System.out.println("Salário: " + this.salario);
+        System.out.println("Disciplina: " + this.disciplina);
     }
 }
 
 class Aluno extends Pessoa {
-    public String matricula;
-    public double nota;
+    private String matricula;
+    private double nota;
+
+    // Getters e Setters
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 
     void adicionarAluno(String nome, String email, String telefone, int idade, char sexo, String cpf, String matricula, double nota) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.idade = idade;
-        this.sexo = sexo;
-        this.cpf = cpf;
+        setNome(nome);
+        setEmail(email);
+        setTelefone(telefone);
+        setIdade(idade);
+        setSexo(sexo);
+        setCpf(cpf);
         this.matricula = matricula;
         this.nota = nota;
     }
@@ -62,21 +144,30 @@ class Aluno extends Pessoa {
 }
 
 class Secretario extends Pessoa {
-    public double salario;
+    private double salario;
+
+    // Getters e Setters
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 
     void adicionarSecretario(String nome, String email, String telefone, int idade, char sexo, String cpf, double salario) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.idade = idade;
-        this.sexo = sexo;
-        this.cpf = cpf;
+        setNome(nome);
+        setEmail(email);
+        setTelefone(telefone);
+        setIdade(idade);
+        setSexo(sexo);
+        setCpf(cpf);
         this.salario = salario;
     }
 
     void exibirDados() {
         super.exibirDados();
-        System.out.print("Salário: " + this.salario);
+        System.out.println("Salário: " + this.salario);
     }
 }
 
@@ -87,19 +178,4 @@ public class Main {
         Secretario secretario = new Secretario();
 
         // Classe professor
-        professor.adicionarProfessor("Fábio","fabio@unip.com","16 99207-9573", 38, 'M', "5037446805", 5.013, "Lógica matemática");
-        professor.exibirDados();
-
-        System.out.println("======================================");
-
-        // Classe aluno
-        aluno.adicionarAluno("Eduardo", "ef09042@gmail.com", "17 99183-3981", 19, 'M', "93938985100", "R024FI0", 9.3);
-        aluno.exibirDados();
-
-        System.out.println("======================================");
-
-        // Classe secretario
-        secretario.adicionarSecretario("José","jose@secretario.com","16 992134-9573", 34, 'M', "503446805", 10.313);
-        secretario.exibirDados();
-    }
-}
+        professor.adicionarProfessor("Fábio", "
